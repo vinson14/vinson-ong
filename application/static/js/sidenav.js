5ffb29a8e95f99f606e04ab7e40066a63f955a8d
Vinson <vinson1493@gmail.com>
2020-07-23 09:01:12 +0800

    Show page only when loaded

--- application/static/js/sidenav.js
+++ application/static/js/sidenav.js
@@ -3,5 +3,10 @@ function toggleSidebar() {
   sidebar.classList.toggle("sidebar-active");
   var icon = document.getElementById("sidebar-expand-icon");
   icon.classList.toggle("fa-bars");
-  icon.classList.toggle("fa-times")
+  icon.classList.toggle("fa-times");
 }
+
+window.addEventListener("load", function() {
+    body = document.querySelector('body');
+    body.classList.add("fadeIn");
+})
